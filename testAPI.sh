c48658684cd9a9f5fb39262f2b4ec3b2d1bc30b3
PeP <pep@biospherecorp.net>
2017-07-21 22:46:00 +0200

    initial commit

---
+++ testAPI.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+#
+# .: THE SCRIPT : .
+#
+#
+# The expected response from
+# the ping route is an object
+# like so :
+#
+# {
+#   "response":"PONG"
+# }
+#
+# Source the settings file
+source ./CONFIG_FILE 
+
+#
+#
+# The result of calling the API
+# with the requested paramters,
+# here it should be the string 'PONG'
+result="$(curl -s -X $queryMethod -H "Accept: application/json" $url | jq -r '.'$jsonKey)"
+
+# Check the result of calling the API
+if [ "$result" = 'PONG' ]
+then
+	echo "TRUE"
+	exit 0
+else
+	echo "FALSE"
+	exit 1
+fi
