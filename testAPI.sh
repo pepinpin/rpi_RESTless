2db0a2cbe85df0c0dd2c6a4e611139898042365d
PeP <pep@biospherecorp.net>
2017-07-21 23:08:29 +0200

    some refactoring/cleaning refactoring

--- testAPI.sh
+++ testAPI.sh
@@ -7,12 +7,11 @@
 # Source the settings file
 source ./CONFIG_FILE 
 #
-#
 # The result of calling the API
 # with the requested paramters,
 # here it should be the string 'PONG'
 result="$(curl -s -X $queryMethod -H "Accept: application/json" $url | jq -r '.'$jsonKey)"
-
+#
 # Check the result of calling the API
 if [ "$result" = 'PONG' ]
 then
@@ -22,3 +21,5 @@ else
 	echo "FALSE"
 	exit 1
 fi
+#
+#
