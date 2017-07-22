f26d3e6c07ed669bab6e1949f3a4314aa5f477fc
pierre-eddy.pinsolle <pierre-eddy.pinsolle@batiment.setec.fr>
2017-07-22 22:45:02 +0200

    the config file path is dynamicly set

--- scripts/testAPI.sh
+++ scripts/testAPI.sh
@@ -3,27 +3,41 @@
 # .: THE SCRIPT : .
 #
 #
-# Source the settings file
-source ./CONFIG_FILE 
-read isOnline < ./IS_ONLINE
+# set the CONFIG_FILE variable with 
+# the path to the CONFIG_FILE file
+CONFIG_FILE="$( dirname $( dirname $( realpath $0 ) ) )/CONFIG_FILE"
+# test if the file exist
+if [ -f $CONFIG_FILE  ]
+# if it does
+then
+	# source it
+	source $CONFIG_FILE
+ 
+# if it doesn't exist
+else
+	# exit with an error
+	exit 1 # the file doesn't exist
+fi
+
 #
 # The result of calling the API
 # with the requested paramters,
 # here it should be the string 'PONG'
 result="$(curl -s -X $queryMethod -H "Accept: application/json" $url | jq -r '.'$jsonKey)"
 #
-# Check the result of calling the API
-if [ "$result" = "$jsonValue" ]
+# Check if the CONFIG_FILE exists (to avoid side effects) and
+# test the result of calling the API
+if [[ -f $CONFIG_FILE && "$result" = "$jsonValue" ]]
 then
 	echo "target is ..:: ONLINE ::.."
-	isOnline='true'
+#	isOnline='true'
 else
 	echo 'target is /!\/!\ :OFFLINE: /!\/!\'
-	isOnline='false'
+#	isOnline='false'
 fi
 #
 # write the value of the isOnline variable to the IS_ONLINE file
-echo $isOnline > ./IS_ONLINE
+#echo $isOnline > ./IS_ONLINE
 #
 # And exit gracefully
 exit 0
