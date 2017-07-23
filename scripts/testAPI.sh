b26a341f1fe1c60d3e754c8a4e62b8778ccada66
pierre-eddy.pinsolle <pierre-eddy.pinsolle@batiment.setec.fr>
2017-07-23 11:48:29 +0200

    some code cleanup

--- scripts/testAPI.sh
+++ scripts/testAPI.sh
@@ -3,9 +3,11 @@
 # .: THE SCRIPT THAT TESTS THE API :.
 #
 #
+
 # set the CONFIG_FILE variable with 
 # the path to the CONFIG_FILE file
 CONFIG_FILE="$( dirname $( dirname $( realpath $0 ) ) )/CONFIG_FILE"
+
 # test if the file exist
 if [ -f $CONFIG_FILE  ]
 # if it does
@@ -18,21 +20,17 @@ else
 	# exit with an error
 	exit 1 # the file doesn't exist
 fi
-#
-#
+
+
 # The result of calling the API
 # with the requested parameters,
 # here it should be the string 'PONG'
 result="$(curl -s -X $queryMethod -H "Accept: application/json" $url | jq -r '.'$jsonKey)"
-#
+
 # test the result of calling the API
 if [ "$result" = "$jsonValue" ]
 then
-	# for debug
-	#echo "target is ..:: ONLINE ::.."
 	exit 0 # the target has been reached
 else
-	#for debug
-	#echo 'target is /!\/!\ :OFFLINE: /!\/!\'
 	exit 2 # the target can't be reached
 fi
